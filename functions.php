30eedf1596b4fbfa50776339d3f7cd91d89bd9e3
Phil Hendricks <phendricks@centralriversaea.org>
2025-10-03 08:45:28 -0500

    removed unneeded end php

--- functions.php
+++ functions.php
@@ -6,4 +6,5 @@ function iowa_aea_child_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), wp_get_theme()->get('Version') );
 }
-?>
+
+
