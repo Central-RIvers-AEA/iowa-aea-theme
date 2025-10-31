68ddcde61a1f19fc4ff8353027611181799837c1
Phil Hendricks <phendricks@centralriversaea.org>
2025-10-31 11:10:34 -0500

    disable next by default

--- blocks/build/events-list/render.php
+++ blocks/build/events-list/render.php
@@ -24,7 +24,7 @@
       <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/>
     </svg>
   </button>
-  <button class='btn-control load-next' data-wp-on--click="callbacks.scrollEvents" aria-label='Show Next Event'>
+  <button class='btn-control load-next' data-wp-on--click="callbacks.scrollEvents" disabled=true aria-label='Show Next Event'>
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
       <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
     </svg>
