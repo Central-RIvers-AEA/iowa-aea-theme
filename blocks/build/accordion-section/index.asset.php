ceb123ddf868a804f320d89937a5d5f62a40f76e
Phil Hendricks <phendricks@centralriversaea.org>
2025-11-21 15:14:34 -0600

    fixed editor for accordion

--- blocks/build/accordion-section/index.asset.php
+++ blocks/build/accordion-section/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '4cbe37bbdad085e414a9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '51454c16ef1f8b9004df');
