7722eb2d67ddd50de7d26a37742e972ab9d736a5
Phil Hendricks <phendricks@centralriversaea.org>
2026-04-07 08:18:03 -0500

    fixing shrinking issue on edit

--- blocks/build/header-slider/index.asset.php
+++ blocks/build/header-slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c656517d230067369be8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ecac338afc4e0c4fef63');
