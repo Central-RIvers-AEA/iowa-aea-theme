fd942ac83bd0c14178fd35e8812c63cf8b02e43e
Phil Hendricks <phendricks@centralriversaea.org>
2026-05-07 09:26:38 -0500

    added deep nesting children

--- blocks/build/page-children/render.php
+++ blocks/build/page-children/render.php
@@ -29,9 +29,13 @@ if(!function_exists('get_top_parent_id')){
 
 	function has_current_page_in_children($children, $current_post_id) {
 		foreach ($children as $child) {
-			if ($child->ID == $current_post_id) {
-				return true;
-			}
+				if ($child['page']->ID == $current_post_id) {
+						return true;
+				}
+				// Recursively check grandchildren
+				if (!empty($child['children']) && has_current_page_in_children($child['children'], $current_post_id)) {
+						return true;
+				}
 		}
 		return false;
 	}
@@ -49,23 +53,57 @@ if(!function_exists('get_top_parent_id')){
 		$hierarchy = array();
 		
 		foreach ($children as $child) {
-			$child_data = array(
-				'page' => $child,
-				'children' => get_children(array(
-					'post_parent' => $child->ID,
-					'post_type'   => 'page',
-					'post_status' => 'publish',
-					'orderby'     => 'menu_order',
-					'order'       => 'ASC'
-				))
-			);
-			$hierarchy[] = $child_data;
-		}
-		
+        $hierarchy[] = array(
+            'page'     => $child,
+            'children' => get_page_current_hierarchy($child->ID) // 👈 recursive call
+        );
+    }
+    
 		return $hierarchy;
 	}
 
+	function render_page_hierarchy($items, $current_post_id, $depth = 0) {
+		if (empty($items)) return;
+		$ul_class = $depth === 0 ? 'parent-pages' : 'child-pages';
+		echo '<ul class="' . esc_attr($ul_class) . '">';
+		foreach ($items as $item) {
+			$page           = $item['page'];
+			$children       = $item['children'];
+			$is_current     = ($page->ID == $current_post_id);
+			$has_current    = has_current_page_in_children($children, $current_post_id);
+			$should_be_open = $is_current || $has_current;
+			$li_class       = $depth === 0 ? 'parent-page-item' : 'child-page-item';
+			$a_class        = $depth === 0 ? 'parent-page-link' : 'child-page-link';
 
+			echo '<li class="' . esc_attr($li_class) . '">';
+
+			if (!empty($children)) {
+				echo '<div class="parent-dropdown' . ($should_be_open ? ' open' : '') . '">';
+			} else {
+				echo '<div class="parent-dropdown">';
+			}
+
+			echo '<a href="' . esc_url(get_permalink($page->ID)) . '" class="' . esc_attr($a_class) . ($is_current ? ' current-page' : '') . '">'
+					. esc_html($page->post_title) . '</a>';
+
+			if (!empty($children)) {
+				if ($should_be_open) {
+					echo '<button type="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-up" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/></svg></button>';
+				} else {
+					echo '<button type="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/></svg></button>';
+				}
+			}
+
+			echo '</div>';
+
+			if (!empty($children)) {
+				render_page_hierarchy($children, $current_post_id, $depth + 1);
+			}
+
+			echo '</li>';
+		}
+		echo '</ul>';
+	}
 }
 
 // Get the current post ID
@@ -85,63 +123,15 @@ if ($is_current_page_in_section) {
 	$page_hierarchy = null;
 }
 
+
+
 ?>
 
 <div <?php echo get_block_wrapper_attributes(); ?>>
 	<?php if ($page_hierarchy): ?>
-			<nav class="page-hierarchy" aria-label="Page Navigation">
-				<ul class="parent-pages">
-					<?php foreach ($page_hierarchy as $parent_item): ?>
-						<?php 
-						// Check if current page is this parent or one of its children
-						$is_current_parent = ($parent_item['page']->ID == $current_post_id);
-						$has_current_child = has_current_page_in_children($parent_item['children'], $current_post_id);
-						$should_be_open = $is_current_parent || $has_current_child;
-						?>
-						<li class="parent-page-item">
-							<?php if ($parent_item['children']): ?>
-								<div class='parent-dropdown<?php echo $should_be_open ? ' open' : ''; ?>'>
-									<a href="<?php echo esc_url(get_permalink($parent_item['page']->ID)); ?>" 
-										class="parent-page-link<?php echo $is_current_parent ? ' current-page' : ''; ?>">
-										<?php echo esc_html($parent_item['page']->post_title); ?>
-									</a>
-
-									<button type='button'>
-										<?php 
-									    if($should_be_open) {
-												echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-up" viewBox="0 0 16 16">
-												<path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/>
-												</svg>';
-											} else {
-												echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
-												</svg>';
-											}?></button>
-								</div>
-							<?php else: ?>
-								<div class='parent-dropdown'>
-									<a href="<?php echo esc_url(get_permalink($parent_item['page']->ID)); ?>" 
-										class="parent-page-link<?php echo $is_current_parent ? ' current-page' : ''; ?>">
-										<?php echo esc_html($parent_item['page']->post_title); ?>
-									</a>
-								</div>
-							<?php endif; ?>
-							<?php if ($parent_item['children']): ?>
-								<ul class="child-pages">
-									<?php foreach ($parent_item['children'] as $child): ?>
-										<li class="child-page-item">
-											<a href="<?php echo esc_url(get_permalink($child->ID)); ?>" 
-												class="child-page-link<?php echo ($child->ID == $current_post_id) ? ' current-page' : ''; ?>">
-												<?php echo esc_html($child->post_title); ?>
-											</a>
-										</li>
-									<?php endforeach; ?>
-								</ul>
-							<?php endif; ?>
-						</li>
-					<?php endforeach; ?>
-				</ul>
-			</nav>
+		<nav class="page-hierarchy" aria-label="Page Navigation">
+				<?php render_page_hierarchy($page_hierarchy, $current_post_id); ?>
+		</nav>
 	<?php else: ?>
 		<?php if (!$is_current_page_in_section): ?>
 			<p class="no-children">This page is not part of a navigation section.</p>
